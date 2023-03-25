39877bc2f0cbc0fa4c001fbdfc2f2ffdc97536e3
Patryk <leniwysid@gmail.com>
2023-03-25 18:59:05 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,49 +1,94 @@
 import React from "react";
 import { useState } from "react";
+import foto1 from "../src/assets/healthy.png";
+import foto2 from "../src/assets/under.png";
+import foto3 from "../src/assets/over.png";
 
 function App() {
-  const [weight, setWeight] = useState(0);
-  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
   const [bmi, setBmi] = useState("");
   const [message, setMessage] = useState("");
 
-  let imgSrc = "";
+  let calcBmi = (e) => {
+    e.preventDefault();
+
+    if (weight === 0 || height === 0) {
+      alert("Please enter a valid values");
+    } else {
+      let bmi = weight / ((height * height) / 10000);
+      setBmi(bmi.toFixed(2));
+
+      if (bmi < 18.5) {
+        setMessage("You are underweight");
+      } else if (bmi >= 18.5 && bmi <= 24.9) {
+        setMessage("Your weight is perfect");
+      } else {
+        setMessage("You are overweight");
+      }
+    }
+  };
+
+  let imgSrc;
+  if (bmi < 1) {
+    imgSrc = null;
+  } else {
+    if (bmi < 18.5) {
+      imgSrc = foto2;
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      imgSrc = foto1;
+    } else {
+      imgSrc = foto3;
+    }
+  }
+
+  const clear = () => {
+    window.location.reload();
+  };
 
   return (
-    <div className="h-screen bg-gray-100 flex justify-center items-center">
+    <div className="w-full h-screen bg-gray-100 flex justify-center items-center">
       <div className="">
-        <div className="rounded-xl bg-gray-200 p-2 shadow-xl flex flex-col items-center">
+        <div className="rounded-xl bg-gray-200 p-2 shadow-xl flex flex-col items-center w-[210px]">
           <div>
             <h1 className="font-bold">BMI Calculator</h1>
           </div>
-          <div className="flex flex-col items-center">
+          <div className=" flex flex-col items-center my-2 ">
             <label className="p-2">Weight</label>
             <input
-              className="rounded-lg text-center"
+              className="rounded-lg text-center "
               placeholder="KG"
               value={weight}
+              onChange={(e) => setWeight(e.target.value)}
             ></input>
             <label className="p-2">Height</label>
             <input
               className="rounded-lg text-center"
               placeholder="CM"
               value={height}
+              onChange={(e) => setHeight(e.target.value)}
             ></input>
           </div>
-          <div className="flex flex-col p-2 w-full">
-            <button className="my-2 border border-orange-500 bg-orange-500 rounded-lg">
+          <div className="flex flex-col p-2 w-full my-2">
+            <button
+              className="my-2 p-2 border border-orange-500 hover:bg-orange-700 duration-300 bg-orange-500 rounded-lg"
+              onClick={calcBmi}
+            >
               Submit
             </button>
-            <button className="border border-orange-500 rounded-lg">
+            <button
+              className="border p-2  border-orange-500 rounded-lg"
+              onClick={clear}
+            >
               Clear
             </button>
           </div>
-          <div>
-            <p>Your BMI is:{bmi}</p>
+          <div className="font-bold flex flex-col items-center">
+            <p>Your BMI is: {bmi}</p>
             <p>{message}</p>
           </div>
           <div>
-            <img src={imgSrc} alt="."></img>
+            <img src={imgSrc} alt=""></img>
           </div>
         </div>
       </div>
